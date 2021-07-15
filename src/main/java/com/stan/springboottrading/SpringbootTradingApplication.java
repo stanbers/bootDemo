42aaf48d49a15bc2976c9a092fb330681931041e
stanxu <stanxu526@gmail.com>
2021-07-15 22:31:10 +0800

    init project

---
+++ src/main/java/com/stan/springboottrading/SpringbootTradingApplication.java
@@ -0,0 +1,13 @@
+package com.stan.springboottrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTradingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stan/springboottrading/SpringbootTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stan.springboottrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTradingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
